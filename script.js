897e79589370a8aa86a17243724d3f850d0daf15
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-24 23:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function insert_Row() {
     const table = document.getElementById("sampleTable");
-     const newRow = table.insertRow(-1);
+     const newRow = table.insertRow(0);
     
     const rowCount = table.rows.length;
     
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
     
-    cell1.innerHTML = "Row" + rowCount + " New Cell1";
-    cell2.innerHTML = "Row" + rowCount + " New Cell2"; 
+    cell1.innerHTML =  "New Cell1";
+    cell2.innerHTML =  "New Cell2"; 
   
 }
